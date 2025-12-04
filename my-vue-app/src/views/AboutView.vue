9e6b4a300bd5b4b8224964c55fb76142bf078deb
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-12-04 11:39:54 +0600

    first commit

---
+++ my-vue-app/src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>All about this website.</p>
+  </div>
+</template>
---
+++ my-vue-app/src/views/ContactView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Contact Page</h1>
+    <p>Contact us at: contact@example.com</p>
+  </div>
+</template>
