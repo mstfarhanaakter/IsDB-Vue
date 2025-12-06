88cc6b7c9bc69291f78d672fb575288d8c99fcf4
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-12-06 12:32:52 +0600

    new

--- my-vite-vue/src/App.vue
+++ my-vite-vue/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
+
 import Home from './components/Dash.vue';
 import Trees from './components/Trees.vue';
 </script>
@@ -11,6 +12,6 @@ import Trees from './components/Trees.vue';
   <Home></Home>
   <Trees></Trees>
   <Footer></Footer>
+  
+
 </template>
-
-
